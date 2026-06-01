8b6543331d38c8d350312139444551e861227806
lucamorino <morinoluc@gmail.com>
2026-06-01 21:05:40 +0200

    v2

--- node-max_player.js
+++ node-max_player.js
@@ -1,5 +1,5 @@
 // change process.cwd() to soundworks application root
-process.chdir('../B3-MAX');
+process.chdir('../B3_H4RSH');
 console.log('node running in:', process.cwd());
 // import the client source file
-import('../B3-MAX/src/clients/max_player.js');
+import('../B3_H4RSH/src/clients/max_player.js');
